5ff3fad5cf1a17f48daeb7ab1b39451c11f4e2f6
Arvin Zoleta <arvin_zoleta@dlsl.edu.ph>
2025-09-08 15:22:44 +0800

    feat: update index.html title and remove favicon link; clean up index.js and add CSS import in AddStudentPage

--- safetag/src/screens/AddStudentPage.js
+++ safetag/src/screens/AddStudentPage.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/UserPage.css';
+import '../css/AddStudentPage.css';
 import  { useState } from "react";
 
 
